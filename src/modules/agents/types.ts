1d1769a774a8d6c91d8ca5f8d8455c95497169e7
thawit <155454715+tacesept@users.noreply.github.com>
2026-01-10 13:45:19 +0700

    18: meetings data table

--- src/modules/agents/types.ts
+++ src/modules/agents/types.ts
@@ -1,4 +1,6 @@
 import type { AppRouter } from "@/trpc/routers/_app";
 import { inferRouterOutputs } from "@trpc/server";
 
+export type AgentGetMany =
+  inferRouterOutputs<AppRouter>["agents"]["getMany"]["items"];
 export type AgentGetOne = inferRouterOutputs<AppRouter>["agents"]["getOne"];
